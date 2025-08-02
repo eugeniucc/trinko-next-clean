9ce8edb8fe299e67b1a91668ae953020c5151812
eugeniucc <eugeniucoloteniucc@gmail.com>
2025-08-02 23:23:17 +0300

    added gtag

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -52,12 +52,34 @@ export default async function LocaleLayout({ children, params }: LayoutProps) {
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5JSL5HH7');`
+          }}
+        />
       </head>
+
       <body className={`${roboto.variable} ${montserrat.variable} text-sm`}>
         <NextIntlClientProvider>
           <HeaderMenu />
           <ClientWrapper>{children}</ClientWrapper>
         </NextIntlClientProvider>
+
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5JSL5HH7"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
       </body>
     </html>
   )
