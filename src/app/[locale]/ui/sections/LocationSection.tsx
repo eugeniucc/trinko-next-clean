a7047ffd6a6559113098dd98fdc87b9e1b8c08b6
eugeniucc <eugeniucoloteniucc@gmail.com>
2025-08-10 23:44:33 +0300

    map inview added

--- src/app/[locale]/ui/sections/LocationSection.tsx
+++ src/app/[locale]/ui/sections/LocationSection.tsx
@@ -1,15 +1,21 @@
 'use client'
 
 import dynamic from 'next/dynamic'
+import { useInView } from 'react-intersection-observer'
 import { CustomLoading } from '@/components/custom/CustomLoading'
 
-const Map = dynamic(() => import('@/components/custom/MapLibre'), { ssr: false, loading: () => <CustomLoading /> })
+const Map = dynamic(() => import('@/components/custom/MapLibre'), {
+  ssr: false,
+  loading: () => <CustomLoading />
+})
 
 export const LocationSection = () => {
+  const { ref, inView } = useInView({ triggerOnce: true, rootMargin: '200px' })
+
   return (
-    <div>
+    <div ref={ref} className="min-h-[400px]">
       <p>LocationSection</p>
-      <Map />
+      {inView && <Map />}
     </div>
   )
 }
