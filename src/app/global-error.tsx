4b077d337831285b3edaa2b444619231ebc4fa6a
eugeniucc <eugeniucoloteniucc@gmail.com>
2025-08-10 13:50:46 +0300

    added sentry for edge/server

---
+++ src/app/global-error.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import * as Sentry from '@sentry/nextjs'
+import NextError from 'next/error'
+import { useEffect } from 'react'
+
+export default function GlobalError({ error }: { error: Error & { digest?: string } }) {
+  useEffect(() => {
+    Sentry.captureException(error)
+  }, [error])
+
+  return (
+    <html>
+      <body>
+        <h1>Something went wrong</h1>
+        <NextError statusCode={0} />
+      </body>
+    </html>
+  )
+}
