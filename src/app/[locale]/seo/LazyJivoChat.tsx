cdd28ef130ffdc06fc98ea73b255a11d4c458249
eugeniucc <eugeniucoloteniucc@gmail.com>
2026-01-05 00:03:39 +0200

    changed jivochat link

--- src/app/[locale]/seo/LazyJivoChat.tsx
+++ src/app/[locale]/seo/LazyJivoChat.tsx
@@ -6,7 +6,7 @@ export const LazyJivoChat = () => {
   useEffect(() => {
     const timeout = setTimeout(() => {
       const script = document.createElement('script')
-      script.src = 'https://code.jivosite.com/widget/z65l5BJYyj'
+      script.src = 'https://code.jivosite.com/widget/w9UCfRlMLQ'
       script.async = true
       document.body.appendChild(script)
     }, 3000)
