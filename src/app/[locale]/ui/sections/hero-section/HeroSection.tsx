c562f6213bc94cd080296df88874fe6ccc5e7f96
eugeniucc <eugeniucoloteniucc@gmail.com>
2026-03-01 22:32:09 +0200

    main page lcp improvements

--- src/app/[locale]/ui/sections/hero-section/HeroSection.tsx
+++ src/app/[locale]/ui/sections/hero-section/HeroSection.tsx
@@ -1,13 +1,10 @@
-import dynamic from 'next/dynamic'
-import Loading from '@/app/loading'
-
-const HeroSlider = dynamic(() => import('./HeroSlider'), {
-  loading: () => <Loading />
-})
+import Image from 'next/image'
+import HeroSlider from './HeroSlider'
 
 export const HeroSection = async () => {
   return (
     <section className="relative min-h-screen w-full">
+      <Image src="/images/homePage/hero-section/hero-1.webp" alt="тату катаны на руке" fill priority sizes="100vw" />
       <HeroSlider />
     </section>
   )
